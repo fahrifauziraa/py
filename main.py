0860017eaa3375ed725e6af98241c4bdf089b3a0
fahrifauziraa <103209183+fahrifauziraa@users.noreply.github.com>
2025-10-03 09:10:30 +0700

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+# Program Hello World sederhana
+print("Halo, Python!")
+
+# Program menyapa pengguna
+# Meminta input nama
+nama = input("Masukkan nama kamu: ")
+
+# Menampilkan sapaan
+print("Halo,", nama + "!")
