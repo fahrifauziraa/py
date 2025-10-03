f8e7ecb02fa3ac8ecd4d2ea9374c4f27be64ae3d
fahrifauziraa <103209183+fahrifauziraa@users.noreply.github.com>
2025-10-03 09:12:16 +0700

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,11 @@
+# Program menghitung luas persegi panjang
+
+# Input panjang dan lebar
+panjang = float(input("Masukkan panjang (cm): "))
+lebar = float(input("Masukkan lebar (cm): "))
+
+# Menghitung luas
+luas = panjang * lebar
+
+# Menampilkan hasil
+print("Luas persegi panjang adalah:", luas, "cm²")
