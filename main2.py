a2d9d31a303d9fa795e3d28009135430198bdc24
fahrifauziraa <103209183+fahrifauziraa@users.noreply.github.com>
2025-10-03 09:11:08 +0700

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,18 @@
+# Program Hello World sederhana
+print("Halo, Python!")
+
+# Program menyapa pengguna
+
+# Meminta input nama
+nama = input("Masukkan nama kamu: ")
+
+# Meminta input kelas
+kelas = input("Masukkan kelas kamu: ")
+
+# Meminta input mata pelajaran
+mapel = input("Masukkan mata pelajaran: ")
+
+# Menampilkan sapaan
+print("Halo,", nama + "!")
+print("Kelas :", kelas)
+print("Mata Pelajaran :", mapel)
